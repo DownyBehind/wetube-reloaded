2afdc81c437fbe02b8aa8f03ce5caecacf963e92
downy <TeamBehindDowny@gmail.com>
2021-10-15 00:14:15 +0900

    complete day 2

--- src/server.js
+++ src/server.js
@@ -1,3 +1,17 @@
 import express from "express";
 
-const app = express();
+const PORT = 4000;
+
+const app = express(); // making application
+
+// configuration application
+
+const handleHome = () => console.log("sombody is trying to go home.");
+
+app.get("/", handleHome);
+
+const handleListening = () =>
+  console.log(`Server listening on port http://localhost:${PORT}🚀`); // open application and listen
+// callback function
+
+app.listen(PORT, handleListening);
