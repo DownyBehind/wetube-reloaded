30bfc3dea2eeac6058194fde2d529cc9e6e011e5
downy <TeamBehindDowny@gmail.com>
2021-11-11 00:48:55 +0900

    what is middleware and how to use middleware

--- src/server.js
+++ src/server.js
@@ -6,16 +6,40 @@ const app = express(); // making application
 
 // configuration application
 
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
+
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") return res.send("<h1>Not Allowed</h1>");
+  console.log("You may allowed");
+  next();
+};
+
+const routerLogger = (req, res, next) => {
+  console.log(req.path);
+  next();
+};
+
+const methodLogger = (req, res, next) => {
+  console.log(req.method);
+  next();
+};
+
 const handleHome = (req, res) => {
-  return res.send("i still love you");
+  return res.send("I love middlewares");
 };
 
-const handleLogin = (req, res) => {
-  return res.send("Login here");
+const handleProtected = (req, res) => {
+  return res.send("Welcome to the private lounge");
 };
 
-app.get("/", handleHome); // how to response
-app.get("/login", handleLogin);
+app.use(logger);
+app.use(privateMiddleware);
+app.get("/", methodLogger, routerLogger, handleHome); // how to response
+app.get("/protected", handleProtected);
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}🚀`); // open application to the outside of world and listen
