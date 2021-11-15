c94d7043ee8a4754a4db48dbf0ba1b99bacb0cda
downy <TeamBehindDowny@gmail.com>
2021-11-15 23:37:17 +0900

    modify folder tree

--- README.md
+++ README.md
@@ -7,3 +7,17 @@
 
 3. making route
 ```
+
+/ -> Home // we call this Global router
+/join -> Join
+/login -> Login
+/search -> Search
+
+/users/edit -> Edit user
+/users/delete -> Delete user
+
+/videos/watch -> Watch Video
+/videos/edit -> Edit video
+/videos/delete -> Delete Video
+/videos/comments -> Comment on a video
+/videos/comments/delete -> Delete a Comment of a Videos
--- src/server.js
+++ src/server.js
@@ -1,45 +1,20 @@
 import express from "express";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import morgan from "morgan"; // for middleware
 
 const PORT = 4000;
 
 const app = express(); // making application
+const logger = morgan("dev");
+app.use(logger);
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 // configuration application
-
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-};
-
-const privateMiddleware = (req, res, next) => {
-  const url = req.url;
-  if (url === "/protected") return res.send("<h1>Not Allowed</h1>");
-  console.log("You may allowed");
-  next();
-};
-
-const routerLogger = (req, res, next) => {
-  console.log(req.path);
-  next();
-};
-
-const methodLogger = (req, res, next) => {
-  console.log(req.method);
-  next();
-};
-
-const handleHome = (req, res) => {
-  return res.send("I love middlewares");
-};
-
-const handleProtected = (req, res) => {
-  return res.send("Welcome to the private lounge");
-};
-
-app.use(logger);
-app.use(privateMiddleware);
-app.get("/", methodLogger, routerLogger, handleHome); // how to response
-app.get("/protected", handleProtected);
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}🚀`); // open application to the outside of world and listen
